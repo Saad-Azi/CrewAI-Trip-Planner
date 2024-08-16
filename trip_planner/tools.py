ba9a44ca58879e53be0456d82b134f4940de82c2
Saad-Azi <sunnyaziz120@gmail.com>
2024-08-16 12:46:02 +0500

    add comments to tools.py

--- trip_planner/tools.py
+++ trip_planner/tools.py
@@ -2,8 +2,10 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.tools import tool
 from dotenv import load_dotenv
 
+# Load environment variables from a .env file
 load_dotenv()
 
+# create custom tool with a tool decorator from langchain
 @tool("Calculate")
 def Calculate(expression):
     """Use this tool to calculate the expenses safely. This tool only excepts a mathematical expression."""
@@ -13,4 +15,5 @@ def Calculate(expression):
     except Exception as e:
         return f"Error in calculation: {e}"
 
+# create an instance of Tavily tool
 search_tool=TavilySearchResults()
